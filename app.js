9046a77387c7882b82efa819cfaed93a6e3ee13d
Martin Sonc <martin.sonc@gmail.com>
2016-04-13 16:08:45 +0100

    logging

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 'use strict';
 
 var express = require('express');
+var logging = require('./lib/logging')();
 
 var app = express();
+
+
+// Add the request logger before anything else so that it can
+// accurately log requests.
+// [START requests]
+app.use(logging.requestLogger);
+// [END requests]
 
 // [START hello_world]
 // Say hello!
 app.get('/', function (req, res) {
   res.status(200).send('Hello, world!');
+  logging.info('Hello, world!');
 });
 // [END hello_world]
 
@@ -20,6 +29,28 @@ app.get('/webhook', function (req, res) {
   }
 });
 // [END webhook_token]
+
+// Add the error logger after all middleware and routes so that
+// it can log errors from the whole application. Any custom error
+// handlers should go after this.
+// [START errors]
+app.use(logging.errorLogger);
+
+// Basic 404 handler
+app.use(function (req, res) {
+  res.status(404).send('Not Found');
+});
+
+// Basic error handler
+app.use(function (err, req, res, next) {
+  /* jshint unused:false */
+  // If our routes specified a specific response, then send that. Otherwise,
+  // send a generic message so as not to leak anything.
+  res.status(500).send(err.response || 'Something broke!');
+});
+// [END errors]
+
+
 
 if (module === require.main) {
   // [START server]
